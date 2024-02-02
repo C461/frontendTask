31c6da4ce9842262c285c7d5e7ba48d6619c99dd
C461 <kozefra@gmai.com>
2024-02-01 22:34:17 -0500

    updating

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,6 +1,6 @@
 import { CreateTask, UpdateTask } from "../interfaces/task.interface";
 
-const API = "http://localhost:4000/api";
+const API = process.env.REACT_APP_API || "http://localhost:4000/api";
 
 export const createTaskRequest = (task: CreateTask) =>
   fetch(`${API}/tasks`, {
